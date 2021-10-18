26d700c2e2bc40005caa84efeef92dacd47f87ec
Anthony Smith <anthony.smith1760@gmail.com>
2021-10-18 01:21:17 -0400

    Added functionality to Chatbox

---
+++ static/scripts/chat.js
@@ -0,0 +1,26 @@
+// Collapsible
+var coll = document.getElementsByClassName('collapsible');
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+}
