8ab8e34a662cf589c4673ac365af23d9d70eb21b
Anthony Smith <anthony.smith1760@gmail.com>
2021-10-21 23:49:39 -0400

    Updated and Finished Code

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,8 +1,8 @@
 // Collapsible
-var coll = document.getElementsByClassName('collapsible');
+var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+    coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
 
         var content = this.nextElementSibling;
@@ -12,6 +12,7 @@ for (let i = 0; i < coll.length; i++) {
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
+
     });
 }
 
@@ -21,17 +22,22 @@ function getTime() {
     minutes = today.getMinutes();
 
     if (hours < 10) {
-        hours = "0" + hours // 05:19 
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
     }
 
     let time = hours + ":" + minutes;
     return time;
 }
 
+// Gets the first message
 function firstBotMessage() {
-    let firstMessage = "How's it going?";
-    document.getElementById("botStartedMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
-    
+    let firstMessage = "How's it going?"
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+
     let time = getTime();
 
     $("#chat-timestamp").append(time);
@@ -40,6 +46,60 @@ function firstBotMessage() {
 
 firstBotMessage();
 
+// Retrieves the response
 function getHardResponse(userText) {
     let botResponse = getBotResponse(userText);
+    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+    $("#chatbox").append(botHtml);
+
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
+
+//Gets the text text from the input box and processes it
+function getResponse() {
+    let userText = $("#textInput").val();
+
+    if (userText == "") {
+        userText = "I love Code Palace!";
+    }
+
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    setTimeout(() => {
+        getHardResponse(userText);
+    }, 1000)
+
+}
+
+// Handles sending text via button clicks
+function buttonSendText(sampleText) {
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    //Uncomment this if you want the bot to respond to this buttonSendText event
+    // setTimeout(() => {
+    //     getHardResponse(sampleText);
+    // }, 1000)
+}
+
+function sendButton() {
+    getResponse();
+}
+
+function heartButton() {
+    buttonSendText("Heart clicked!")
+}
+
+// Press enter to send a message
+$("#textInput").keypress(function (e) {
+    if (e.which == 13) {
+        getResponse();
+    }
+});
